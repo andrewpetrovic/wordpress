8079fbda7dc374dc36b96cd7c5c550d0bb0fec34
andrewpetrovic <andrewpetrovic@gmail.com>
2015-07-20 22:16:40 +0800

    完成 index footer sidebar 学习

--- wp-content/themes/Aurelius/archive.php
+++ wp-content/themes/Aurelius/archive.php
@@ -67,31 +67,5 @@
 		<!-- Blog Navigation -->
 		<p class="clearfix"> <a href="#" class="button float">&lt;&lt; Previous Posts</a> <a href="#" class="button float right">Newer Posts >></a> </p>
 	</div>
-	<!-- Column 2 / Sidebar -->
-	<div class="grid_4">
-		<h4>Catagories</h4>
-		<ul class="sidebar">
-			<li><a href="">So who are we?</a></li>
-			<li><a href="">Philosophy</a></li>
-			<li><a href="">History</a></li>
-			<li><a href="">Jobs</a></li>
-			<li><a href="">Staff</a></li>
-			<li><a href="">Clients</a></li>
-		</ul>
-		<h4>Archives</h4>
-		<ul class="sidebar">
-			<li><a href="">January 2010</a></li>
-			<li><a href="">December 2009</a></li>
-			<li><a href="">Novemeber 2009</a></li>
-			<li><a href="">October 2009</a></li>
-			<li><a href="">September 2009</a></li>
-			<li><a href="">August 2009</a></li>
-		</ul>
-	</div>
-	<div class="hr grid_12 clearfix">&nbsp;</div>
-	<!-- Footer -->
-	<p class="grid_12 footer clearfix"> <span class="float"><strong>Design By</strong> QwibbleDesigns&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>Code By</strong> <a href="http://www.ludou.org/">Ludou</a></span> <a class="float right" href="#">top</a> </p>
-</div>
-<!--end wrapper-->
-</body>
-</html>
+	<?php get_sidebar(); ?>
+	<?php get_footer(); ?>
--- wp-content/themes/Aurelius/single.php
+++ wp-content/themes/Aurelius/single.php
@@ -73,38 +73,5 @@
 			</ul>
 		</form>
 	</div>
-	<!-- Column 2 / Sidebar -->
-	<div class="grid_4">
-		<h4>Catagories</h4>
-		<ul class="sidebar">
-			<li><a href="">So who are we?</a></li>
-			<li><a href="">Philosophy</a></li>
-			<li><a href="">History</a></li>
-			<li><a href="">Jobs</a></li>
-			<li><a href="">Staff</a></li>
-			<li><a href="">Clients</a></li>
-		</ul>
-		<h4>Latest News</h4>
-		<ul class="sidebar">
-			<li><a href="">Chickens rule the world</a></li>
-			<li><a href="">Chuck Bartowski is back!</a></li>
-			<li><a href="">Aurelius for sale</a></li>
-			<li><a href="">Themeforest goes bust</a></li>
-		</ul>
-		<h4>Archives</h4>
-		<ul class="sidebar">
-			<li><a href="">January 2010</a></li>
-			<li><a href="">December 2009</a></li>
-			<li><a href="">Novemeber 2009</a></li>
-			<li><a href="">October 2009</a></li>
-			<li><a href="">September 2009</a></li>
-			<li><a href="">August 2009</a></li>
-		</ul>
-	</div>
-	<div class="hr grid_12 clearfix">&nbsp;</div>
-	<!-- Footer -->
-	<p class="grid_12 footer clearfix"> <span class="float"><strong>Design By</strong> QwibbleDesigns&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>Code By</strong> <a href="http://www.ludou.org/">Ludou</a></span> <a class="float right" href="#">top</a> </p>
-</div>
-<!--end wrapper-->
-</body>
-</html>
+	<?php get_sidebar(); ?>
+	<?php get_footer(); ?>
