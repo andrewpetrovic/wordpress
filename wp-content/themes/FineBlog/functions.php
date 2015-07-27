b8e784b910e4912afab25b892f2e1f1f0da08826
andrewpetrovic <andrewpetrovic@gmail.com>
2015-07-27 10:25:53 +0800

    完善excerpt 字符串截取逻辑

--- wp-content/themes/FineBlog/functions.php
+++ wp-content/themes/FineBlog/functions.php
@@ -1,7 +1,12 @@
 <?php
 function the_excerpt_fine( $more_link_text = null, $strip_teaser = false) {
 	$content = get_the_content( $more_link_text, $strip_teaser );
-	$content = mb_substr($content , 0 , 150);
+	$content_length = mb_strlen($content);
+	$excerpt_length = 150;
+	if ($content_length > $excerpt_length){
+		$content = mb_substr($content , 0 , 150);
+		$content = $content. "...";
+	}
 	/**
 	 * Filter the post content.
 	 *
