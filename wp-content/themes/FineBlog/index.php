9238fb905771ff39904de9bf7de6fbb0e5e0aad8
andrewpetrovic <andrewpetrovic@gmail.com>
2015-07-27 10:00:31 +0800

    FindBlog theme content.php重命名为excerpt.php，index.php中引用excerpt.php为模板

--- wp-content/themes/FineBlog/index.php
+++ wp-content/themes/FineBlog/index.php
@@ -38,7 +38,7 @@ get_header(); ?>
 				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 				 */
 // 				get_template_part( 'content', get_post_format() );
-				get_template_part( 'content');
+				get_template_part( 'excerpt');
 
 			// End the loop.
 			endwhile;
